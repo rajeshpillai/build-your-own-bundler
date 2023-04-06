fa0b5a98163a8ec9c4e2b367b6568cd7860b8076
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-06 06:48:06 +0530

    Add packages

--- bundler.js
+++ bundler.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
+const babylon = require('babylon');
 
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8');
-  console.log(content);
+  const ast = babylon.parse(content, {sourceType: 'module'});
+  console.log(ast);
 }
 
 createAsset('./example/entry.js');
