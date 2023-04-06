5f46622c6400cf0001f5d093badcfcd7ee43253e
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-06 07:02:58 +0530

    Parse dependencies

--- bundler.js
+++ bundler.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const babylon = require('babylon');
 const traverse = require('babel-traverse').default;
+const path = require('path');
 
 let ID = 0;
 
@@ -26,6 +27,24 @@ function createAsset(filename) {
   console.log(deps);
 }
 
-const mainAsset = createAsset('./example/entry.js');
-console.log(mainAsset);
+function createGraph(entry) {
+  const mainAsset = createAsset(entry);
+  
+  const queue = [mainAsset];
+  for(const asset of queue) {
+    const dirname = path.dirname(asset.filename);
+    asset.mapping = {}; // mapping for child assets
+
+    asset.deps.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+      const child = createAsset(absolutePath);
+      asset.mapping[relativePath] = child.id;
+      queue.push(child);
+    });
+  }
+  return queue;
+}
+
+const graph = createGraph('./example/entry.js');
+console.log(graph);
 
