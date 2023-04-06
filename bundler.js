9256a8768bc292d64a077b18b68e140487310b5f
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-06 06:54:33 +0530

    Parse Import declarations

--- bundler.js
+++ bundler.js
@@ -1,10 +1,31 @@
 const fs = require('fs');
 const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
+
+let ID = 0;
 
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8');
   const ast = babylon.parse(content, {sourceType: 'module'});
-  console.log(ast);
+
+  const deps = [];
+
+  traverse(ast, {
+    ImportDeclaration: ({node}) => {
+      deps.push(node.source.value);
+    }
+  });
+
+  const id = ID++;
+
+  return {
+    id,
+    filename,
+    deps
+  };
+  console.log(deps);
 }
 
-createAsset('./example/entry.js');
+const mainAsset = createAsset('./example/entry.js');
+console.log(mainAsset);
+
